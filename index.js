3a6453ad5c96d816465721eef2f81a6386096914
Seungki Hong <tmdrl9913@gmail.com>
2024-10-03 10:10:53 -0700

    view page shows calendar

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express');
 const app = express();
 const initializeDatabase = require('./config/db');
 
+app.set('view engine', 'ejs'); // Set EJS as view engine
+
+
 // Middleware
 app.use(express.json());
+
 
 // Route definition
 app.get('/', (req, res) => {
     res.send('Welcome to Project4800');
+});
+
+app.get('/view', (req, res) => {
+    res.render('view'); // This will render view.ejs
 });
 
 // Server and database initialization
