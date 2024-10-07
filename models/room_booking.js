79a14c1e1d55f1b4ed3b66aeea6a4f8854682215
Ning Feng <nfeng2@my.bcit.ca>
2024-10-07 11:57:41 -0700

    add room_booking.js in models

---
+++ models/room_booking.js
@@ -0,0 +1,53 @@
+const {DataTypes} = require("sequelize");
+const {sequelize} = require("../config/db");
+
+const Room_Booking = sequelize.define(
+    "Room_Booking",
+    {
+        booking_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        booking_status: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        room_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: "Room",
+                key: "room_id"
+            }
+        },
+        course_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: "Course",
+                key: "course_id"
+            }
+        },
+        start_time: {
+            type: DataTypes.DATETIME,
+            allowNull: true
+        },
+        end_time: {
+            type: DataTypes.DATETIME,
+            allowNull: true
+        },
+        booking_date: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        booking_duration: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        tableName: "Room_Booking",
+        timestamps: false
+    }
+);
