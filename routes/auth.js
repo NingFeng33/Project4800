d4df05478e67da14fae2c9d92293a42663fb8f9e
Jun Han <junhohan2@gmail.com>
2024-10-16 07:42:26 -0700

    merge from dev

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
-const {DataTypes, Op} = require('sequelize');
-const { sequelize } = require('../config/db');
+const { DataTypes, Op } = require("sequelize");
+const { sequelize } = require("../config/db");
 const express = require("express");
 const { isAuthenticated } = require("../middleware/authMiddleware");
 const {
@@ -12,8 +12,8 @@ const {
   checkRoomAvailability,
   bookRoom,
 } = require("../controllers/authController");
-const { Program } = require('../models/program'); 
-const { Course } = require('../models/course');
+const { Program } = require("../models/program");
+const { Course } = require("../models/course");
 const router = express.Router();
 
 router.get("/", getLogin);
@@ -36,59 +36,61 @@ router.get("/dashboard", isAuthenticated, (req, res) => {
 });
 
 // Render the booking page
-router.get('/admin/booking', isAuthenticated,(req, res) => {
-  res.render('booking');
+router.get("/admin/booking", isAuthenticated, (req, res) => {
+  res.render("booking");
 });
 
 // API to check room availability
-router.post('/admin/booking/check-availability', isAuthenticated,checkRoomAvailability);
+router.post(
+  "/admin/booking/check-availability",
+  isAuthenticated,
+  checkRoomAvailability
+);
 //router.post('/booking/check-availability', checkRoomAvailability);
 // API to book a room
-router.post('/admin/booking/book-room', isAuthenticated,bookRoom);
-
+router.post("/admin/booking/book-room", isAuthenticated, bookRoom);
 
 // Fetch all programs - API route
-router.get('/api/programs', async (req, res) => {
+router.get("/api/programs", async (req, res) => {
   try {
     const programs = await Program.findAll();
     res.json(programs);
   } catch (error) {
     console.error("Failed to fetch programs:", error);
-    res.status(500).json({ message: 'Failed to fetch programs' });
+    res.status(500).json({ message: "Failed to fetch programs" });
   }
 });
 
 // Endpoint to get courses by program ID
-router.get('/api/courses/:programId', async (req, res) => {
+router.get("/api/courses/:programId", async (req, res) => {
   try {
-      const programId = req.params.programId;
-      const courses = await Course.findAll({
-          where: { program_id: programId }
-      });
-      res.json(courses);
+    const programId = req.params.programId;
+    const courses = await Course.findAll({
+      where: { program_id: programId },
+    });
+    res.json(courses);
   } catch (error) {
-      console.error("Failed to fetch courses:", error);
-      res.status(500).json({ message: 'Failed to fetch courses' });
+    console.error("Failed to fetch courses:", error);
+    res.status(500).json({ message: "Failed to fetch courses" });
   }
 });
 
-router.get('/view', async (req, res) => {
+router.get("/view", async (req, res) => {
   try {
-      await sequelize.authenticate();
-      console.log('Database connected successfully!');
+    await sequelize.authenticate();
+    console.log("Database connected successfully!");
 
-      const programs = await Program.findAll();
-      const courses = await Course.findAll();
+    const programs = await Program.findAll();
+    const courses = await Course.findAll();
 
-      console.log('Programs:', programs);
-      console.log('Courses:', courses);
+    console.log("Programs:", programs);
+    console.log("Courses:", courses);
 
-      res.render('view', { programs, courses });
+    res.render("view", { programs, courses });
   } catch (error) {
-      console.error('Error fetching data from database:', error);
-      res.status(500).send('Error fetching data from database');
+    console.error("Error fetching data from database:", error);
+    res.status(500).send("Error fetching data from database");
   }
 });
-
 
 module.exports = router;
