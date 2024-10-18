52912f6ff31d82b236da1dd7921de2f149f31f55
Jun Han <junhohan2@gmail.com>
2024-10-18 14:20:42 -0700

    add resetRoutes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-process.env.TZ = 'America/Vancouver'
+process.env.TZ = "America/Vancouver";
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
@@ -6,21 +6,20 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const { sequelize, User, Role} = require("./models");
-const authRoutes = require("./routes/auth");
+const { sequelize, User, Role } = require("./models");
+const authRoutes = require("./routes/authRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const programRoutes = require("./routes/programRoutes");
+const resetRoutes = require("./routes/resetRoutes");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 const sessionStore = new SequelizeStore({ db: sequelize });
-
 
 app.use(
   session({
@@ -35,8 +34,10 @@ app.use(
 sessionStore.sync();
 
 app.use(authRoutes);
-app.use(express.static('public'));
-
+app.use(bookingRoutes);
+app.use(programRoutes);
+app.use(resetRoutes);
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 9040;
 app.listen(PORT, () => {
