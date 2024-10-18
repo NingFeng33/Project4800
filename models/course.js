baa1e34ae79300ca747fed4f0d1d97530142e057
Jun Han <junhohan2@gmail.com>
2024-10-18 14:11:06 -0700

    copy from dev

--- models/course.js
+++ models/course.js
@@ -1,60 +1,59 @@
-const {DataTypes} = require("sequelize");
-const {sequelize} = require("../config/db");
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
 
 const Course = sequelize.define(
-    "Course",
-    {
-        course_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        course_code: {
-            type: DataTypes.STRING(255),
-            allowNull: false
-        },
-        course_name: {
-            type: DataTypes.STRING(255),
-            allowNull: false
-        },
-        faculty_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: "Faculty",
-                key: "faculty_id"
-            }
-        },
-        class_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: "Class",
-                key: "class_id"
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: "User",
-                key: "user_id"
-            }
-        },
-        program_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: "Program",
-                key: "program_id"
-            }
-        },
+  "Course",
+  {
+    course_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        tableName: "Courses",
-        timestamps: false
-    }
+    course_code: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    course_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    faculty_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "Faculty",
+        key: "faculty_id",
+      },
+    },
+    class_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "Class",
+        key: "class_id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "User",
+        key: "user_id",
+      },
+    },
+    program_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "Program",
+        key: "program_id",
+      },
+    },
+  },
+  {
+    tableName: "Courses",
+    timestamps: false,
+  }
 );
 
-module.exports = {Course};
-
+module.exports = { Course };
