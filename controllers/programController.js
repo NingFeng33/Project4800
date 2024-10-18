f96230ca0b418c3ea6840c45a794ba32bb6a6483
Jun Han <junhohan2@gmail.com>
2024-10-18 14:54:18 -0700

    move business logic from authController

---
+++ controllers/programController.js
@@ -0,0 +1,40 @@
+const { Program } = require("../models/program");
+const { Course } = require("../models/course");
+
+exports.getPrograms = async (req, res) => {
+  try {
+    const programs = await Program.findAll();
+    res.json(programs);
+  } catch (error) {
+    console.error("Failed to fetch programs:", error);
+    res.status(500).json({ message: "Failed to fetch programs" });
+  }
+};
+
+exports.getCoursesByProgramId = async (req, res) => {
+  try {
+    const programId = req.params.programId;
+    const courses = await Course.findAll({
+      where: { program_id: programId },
+    });
+    res.json(courses);
+  } catch (error) {
+    console.error("Failed to fetch courses:", error);
+    res.status(500).json({ message: "Failed to fetch courses" });
+  }
+};
+
+exports.viewProgramsAndCourses = async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully!");
+
+    const programs = await Program.findAll();
+    const courses = await Course.findAll();
+
+    res.render("view", { programs, courses });
+  } catch (error) {
+    console.error("Error fetching data from database:", error);
+    res.status(500).send("Error fetching data from database");
+  }
+};
