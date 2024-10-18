210f04bfa6dc91626ef8e8a98cce914fec136a78
Jun Han <junhohan2@gmail.com>
2024-10-18 14:52:18 -0700

    copy from dev

--- config/db.js
+++ config/db.js
@@ -1,17 +1,23 @@
-process.env.TZ = 'America/Vancouver';
-console.log("Current timezone set in Node.js:", process.env.TZ);
-console.log("Current time according to Node.js:", new Date().toString());
-console.log("UTC time according to Node.js:", new Date().toUTCString());
+// process.env.TZ = "America/Vancouver";
+// console.log("Current timezone set in Node.js:", process.env.TZ);
+// console.log("Current time according to Node.js:", new Date().toString());
+// console.log("UTC time according to Node.js:", new Date().toUTCString());
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  dialectOptions: {
-    charset: "utf8mb4",
-    timezone: 'local',
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      charset: "utf8mb4",
+      timezone: "local",
+    },
+    logging: false,
+  }
+);
 
 module.exports = { sequelize };
