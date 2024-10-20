76de1abfe0ecbd2c741d00967fc58bfab1196614
ningfeng33 <nfeng2@my.bcit.ca>
2024-10-20 01:36:30 -0700

    modify port

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(authRoutes);
 app.use(express.static('public'));
 
 
-const PORT = process.env.PORT || 9040;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
